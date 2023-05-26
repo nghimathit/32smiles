068d299910a7196f5724e28d64bd8cd392665adb
nghimathit <vannghinguyen152001@gmail.com>
2023-05-26 20:40:47 +0700

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -15,9 +15,38 @@ btn.forEach(function(btn,index){
 })
 function addcart(productImg,productName, productPrice){
      var addtr = document.createElement("tr")
-     var trcontent = '<tr><td style="display: flex; align-items: center;"><img style="width: 100px; height: 100px;" src="'+productImg+'" alt="">'+productName+'</td><td><p><span>'+productPrice+'</span></p></td><td><input style="width: 50px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;">Xoa</td></tr>'
+     var cartItem = document.querySelectorAll("tbody tr");
+     for(var i =0; i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".title");
+        if(productT[i].innerHTML ===productName ){
+            console.log(productT[i])
+            alert("San Pham Da Co Trong Gio, Chon San Pham Khac");
+            return 
+        }
+        
+     }
+
+     var trcontent = '<tr><td style="display: flex; align-items: center;"><img style="width: 100px; height: 100px;" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span>'+productPrice+'</span></p></td><td><input style="width: 50px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;">Xoa</td></tr>'
      addtr.innerHTML=trcontent;
      var cartTable = document.querySelector("tbody");
      cartTable.append(addtr);
+     carttotal()
+}  // tinh tong
+function carttotal(){
+    var cartItem = document.querySelectorAll("tbody tr")
+    var totalB=0;
+    for(var i =0; i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input").value
+        var productPrice = cartItem[i].querySelector("span").innerHTML
+
+        totalA = inputValue*productPrice
+        totalB+=totalA;
+        
+
+    }
+    var cartTotalA = document.querySelector(".price-total span");
+    cartTotalA.innerHTML = totalB.toFixed(2);
+    // console.log(cartTotalA)
+
 }
 
