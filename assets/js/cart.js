0e382d7cfd0acb27858e131d9fa3eb80b134d274
nghimathit <vannghinguyen152001@gmail.com>
2023-05-26 22:15:07 +0700

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -26,21 +26,25 @@ function addcart(productImg,productName, productPrice){
         
      }
 
-     var trcontent = '<tr><td style="display: flex; align-items: center;"><img style="width: 100px; height: 100px;" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span>'+productPrice+'</span></p></td><td><input style="width: 50px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;">Xoa</td></tr>'
+     var trcontent = '<tr><td style="display: flex; align-items: center;"><img style="width: 100px; height: 100px;" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span class="prices">'+productPrice+'</span></p></td><td><input style="width: 50px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="delete">Xoa</span></td></tr>'
      addtr.innerHTML=trcontent;
      var cartTable = document.querySelector("tbody");
      cartTable.append(addtr);
      carttotal()
-}  // tinh tong
+     deleteCart()
+} 
+
+ // tinh tong
 function carttotal(){
     var cartItem = document.querySelectorAll("tbody tr")
     var totalB=0;
     for(var i =0; i<cartItem.length;i++){
         var inputValue = cartItem[i].querySelector("input").value
-        var productPrice = cartItem[i].querySelector("span").innerHTML
+        var productPrice = cartItem[i].querySelector(".prices").innerHTML
 
         totalA = inputValue*productPrice
         totalB+=totalA;
+        inputchange()
         
 
     }
@@ -49,4 +53,34 @@ function carttotal(){
     // console.log(cartTotalA)
 
 }
+// xoa
+function deleteCart(){
+    var cartItem = document.querySelectorAll("tbody tr");
+    for(var i =0; i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".delete");
+        productT[i].addEventListener("click",function(event){
+            var cartDelete = event.target
+            var cartItemR = cartDelete.parentElement.parentElement
+            cartItemR.remove()
+            carttotal()
+            // console.log(cartItemR)
+        }) 
+        }
+     }
+     function inputchange(){
+        var cartItem = document.querySelectorAll("tbody tr");
+        for(var i =0; i<cartItem.length;i++){
+            var inputValue = cartItem[i].querySelector("input");
+            inputValue.addEventListener("change",function(){
+                carttotal()
+            })
+            
+            }
+
+     }
+    var buttonT= document.getElementById("thongbaothanhtoan");
+    buttonT.onclick = function(){
+        alert("Thanh Toan Thanh Cong");
+    }
+    
 
