495521222ea3eebd8e6ef500ac8ce886967fd26b
nghimathit <vannghinguyen152001@gmail.com>
2023-05-27 11:41:23 +0700

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,86 +1,124 @@
-const btn = document.querySelectorAll("i.fas.fa-cart-plus");
-// console.log(btn)
-btn.forEach(function(btn,index){
-    btn.addEventListener("click",function(event){
-        var btnItem = event.target;
-        var product = btnItem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
-        var productImg = product.querySelector("img").src;
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartButtons = document.querySelectorAll("a.btn.btn-success.text-white.mt-2.add-to-cart");
+  
+    addToCartButtons.forEach(function(button) {
+      button.addEventListener("click", function(event) {
+        event.preventDefault();
+        var product = button.closest(".product-wap");
+        var productImg = product.querySelector("img.card-img").src;
         var productName = product.querySelector("a.h3.text-decoration-none").innerText;
         var productPrice = product.querySelector("b").innerText;
-        addcart(productImg,productName, productPrice)
-
-        
-    });    
-
-})
-function addcart(productImg,productName, productPrice){
-     var addtr = document.createElement("tr")
-     var cartItem = document.querySelectorAll("tbody tr");
-     for(var i =0; i<cartItem.length;i++){
-        var productT = document.querySelectorAll(".title");
-        if(productT[i].innerHTML ===productName ){
-            console.log(productT[i])
-            alert("San Pham Da Co Trong Gio, Chon San Pham Khac");
-            return 
+  
+        var cartItem = {
+          img: productImg,
+          name: productName,
+          price: productPrice
+        };
+  
+        var existingCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+        var isDuplicate = existingCartItems.some(function(item) {
+          return item.name === cartItem.name;
+        });
+  
+        if (isDuplicate) {
+          alert("Sản phẩm đã có trong giỏ hàng. Vui lòng chọn sản phẩm khác.");
+          return;
         }
-        
-     }
-
-     var trcontent = '<tr><td style="display: flex; align-items: center;"><img style="width: 100px; height: 100px;" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span class="prices">'+productPrice+'</span></p></td><td><input style="width: 50px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="delete">Xoa</span></td></tr>'
-     addtr.innerHTML=trcontent;
-     var cartTable = document.querySelector("tbody");
-     cartTable.append(addtr);
-     carttotal()
-     deleteCart()
-} 
-
- // tinh tong
-function carttotal(){
-    var cartItem = document.querySelectorAll("tbody tr")
-    var totalB=0;
-    for(var i =0; i<cartItem.length;i++){
-        var inputValue = cartItem[i].querySelector("input").value
-        var productPrice = cartItem[i].querySelector(".prices").innerHTML
-
-        totalA = inputValue*productPrice
-        totalB+=totalA;
-        inputchange()
-        
-
+  
+        existingCartItems.push(cartItem);
+        localStorage.setItem("cartItems", JSON.stringify(existingCartItems));
+        alert("Sản phẩm đã được thêm vào giỏ hàng.");
+        loadCartItems();
+      });
+    });
+  
+    function loadCartItems() {
+        var existingCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+      
+        var cartTable = document.querySelector("tbody");
+        cartTable.innerHTML = "";
+  
+      existingCartItems.forEach(function(item) {
+        var tr = document.createElement("tr");
+        tr.innerHTML = '<td style="display: flex; align-items: center;"><img style="width: 100px; height: 100px;" src="' + item.img + '" alt=""><span class="title">' + item.name + '</span></td><td><p><span class="prices">' + item.price + '</span></p></td><td><input style="width: 50px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="delete">Xóa</span></td>';
+        cartTable.appendChild(tr);
+      });
+      
+      updateItemCount(existingCartItems.length);
+      carttotal();
+      deleteCart();
     }
-    var cartTotalA = document.querySelector(".price-total span");
-    cartTotalA.innerHTML = totalB.toFixed(2);
-    // console.log(cartTotalA)
-
-}
-// xoa
-function deleteCart(){
-    var cartItem = document.querySelectorAll("tbody tr");
-    for(var i =0; i<cartItem.length;i++){
+    document.addEventListener("DOMContentLoaded", function() {
+        var itemCountElement = document.getElementById("item-count");
+        var itemCount = localStorage.getItem("itemCount");
+      
+        if (itemCount) {
+          itemCountElement.textContent = itemCount;
+        }
+      });
+      
+      function updateItemCount(count) {
+        var itemCountElement = document.getElementById("item-count");
+        itemCountElement.textContent = count;
+        localStorage.setItem("itemCount", count);
+      }
+    function carttotal() {
+      var cartItem = document.querySelectorAll("tbody tr");
+      var totalB = 0;
+      for (var i = 0; i < cartItem.length; i++) {
+        var inputValue = cartItem[i].querySelector("input").value;
+        var productPrice = cartItem[i].querySelector(".prices").innerHTML;
+  
+        totalA = inputValue * productPrice;
+        totalB += totalA;
+        inputchange();
+      }
+      var cartTotalA = document.querySelector(".price-total span");
+      cartTotalA.innerHTML = totalB.toFixed(2);
+    }
+  
+    function deleteCart() {
+      var cartItem = document.querySelectorAll("tbody tr");
+      for (var i = 0; i < cartItem.length; i++) {
         var productT = document.querySelectorAll(".delete");
-        productT[i].addEventListener("click",function(event){
-            var cartDelete = event.target
-            var cartItemR = cartDelete.parentElement.parentElement
-            cartItemR.remove()
-            carttotal()
-            // console.log(cartItemR)
-        }) 
-        }
-     }
-     function inputchange(){
-        var cartItem = document.querySelectorAll("tbody tr");
-        for(var i =0; i<cartItem.length;i++){
-            var inputValue = cartItem[i].querySelector("input");
-            inputValue.addEventListener("change",function(){
-                carttotal()
-            })
-            
-            }
-
-     }
-    var buttonT= document.getElementById("thongbaothanhtoan");
-    buttonT.onclick = function(){
-        alert("Thanh Toan Thanh Cong");
+        productT[i].addEventListener("click", function(event) {
+          var cartDelete = event.target;
+          var cartItemR = cartDelete.parentElement.parentElement;
+          cartItemR.remove();
+          carttotal();
+  
+          // Lấy danh sách sản phẩm từ local storage
+          var existingCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  
+          // Lọc ra sản phẩm cần xóa
+          var productName = cartItemR.querySelector(".title").innerText;
+          existingCartItems = existingCartItems.filter(function(item) {
+            return item.name !== productName;
+          });
+  
+          // Cập nhật lại danh sách sản phẩm trong local storage
+          localStorage.setItem("cartItems", JSON.stringify(existingCartItems));
+        });
+      }
     }
+  
+    function inputchange() {
+      var cartItem = document.querySelectorAll("tbody tr");
+      for (var i = 0; i < cartItem.length; i++) {
+        var inputValue = cartItem[i].querySelector("input");
+        inputValue.addEventListener("change", function() {
+          carttotal();
+        });
+      }
+    }
+  
+    var buttonT = document.getElementById("thongbaothanhtoan");
+    buttonT.onclick = function() {
+      alert("Thanh toán thành công");
+    };
+  
+    // Load danh sách sản phẩm từ local storage khi trang được tải
+    loadCartItems();
     
-
+  });
+  
