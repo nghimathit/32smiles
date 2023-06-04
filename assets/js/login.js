08e40331cd9421354c7e3c442e61d00d87c4da68
nghimathit <vannghinguyen152001@gmail.com>
2023-06-05 00:47:22 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,34 +1,39 @@
-console.clear();
-
 const loginBtn = document.getElementById('loginTitle');
 const signupBtn = document.getElementById('signupTitle');
+const usernameElement = document.getElementById('username');
+const loginLink = document.getElementById('loginLink');
+const logoutLink = document.getElementById('logoutLink');
 
 loginBtn.addEventListener('click', (e) => {
   let parent = e.target.parentNode.parentNode;
   Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-	if (element !== "slide-up") {
-	  parent.classList.add('slide-up')
-	} else {
-	  signupBtn.parentNode.classList.add('slide-up')
-	  parent.classList.remove('slide-up')
-	}
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up')
+    } else {
+      signupBtn.parentNode.classList.add('slide-up')
+      parent.classList.remove('slide-up')
+    }
   });
 });
 
 signupBtn.addEventListener('click', (e) => {
   let parent = e.target.parentNode;
   Array.from(e.target.parentNode.classList).find((element) => {
-	if (element !== "slide-up") {
-	  parent.classList.add('slide-up')
-	} else {
-	  loginBtn.parentNode.parentNode.classList.add('slide-up')
-	  parent.classList.remove('slide-up')
-	}
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up')
+    } else {
+      loginBtn.parentNode.parentNode.classList.add('slide-up')
+      parent.classList.remove('slide-up')
+    }
   });
 });
 
-document.getElementById('loginLink').addEventListener('click', function() {
+loginLink.addEventListener('click', function() {
   openPopup();
+});
+
+logoutLink.addEventListener('click', function() {
+  logout();
 });
 
 function openPopup() {
@@ -48,11 +53,12 @@ function closePopup() {
 // Lắng nghe sự kiện khi chuột di chuyển ra khỏi overlay
 document.querySelector('.overlay').addEventListener('click', function(event) {
   if (event.target.classList.contains('overlay')) {
-	closePopup();
+    closePopup();
   }
 });
 
-document.getElementById('signupButton').addEventListener('click', function() {
+document.getElementById('signupButton').addEventListener('click', function(e) {
+  e.preventDefault();
   signUp();
 });
 
@@ -63,30 +69,45 @@ function signUp() {
 
   // Kiểm tra xem đã nhập đủ thông tin chưa
   if (name && email && password) {
-	var user = {
-	  name: name,
-	  email: email,
-	  password: password
-	};
+    var users = JSON.parse(localStorage.getItem('users')) || [];
+    var newUser = {
+      name: name,
+      email: email,
+      password: password
+    };
 
-	// Lưu thông tin người dùng vào local storage
-	localStorage.setItem('user', JSON.stringify(user));
+    // Kiểm tra xem email đã tồn tại hay chưa
+    var existingUser = users.find(function(user) {
+      return user.email === email;
+    });
 
-	// Xoá các input sau khi đăng ký thành công
-	document.getElementById('nameInput').value = '';
-	document.getElementById('emailInput').value = '';
+    if (existingUser) {
+      alert('Email already exists. Please choose a different email.');
+      return;
+    }
+
+    // Thêm người dùng mới vào danh sách
+    users.push(newUser);
+
+    // Lưu danh sách người dùng vào local storage
+    localStorage.setItem('users', JSON.stringify(users));
+
+    // Xoá các input sau khi đăng ký thành công
+    document.getElementById('nameInput').value = '';
+    document.getElementById('emailInput').value = '';
 	document.getElementById('passwordInput').value = '';
 
-	alert('Sign Up Success! Please log in.');
+    alert('Sign Up Success! Please log in.');
 
-	// Hiển thị form đăng nhập
-	showLoginForm();
+    // Hiển thị form đăng nhập
+    showLoginForm();
   } else {
-	alert('Please enter full information.');
+    alert('Please enter full information.');
   }
 }
 
-document.getElementById('loginButton').addEventListener('click', function() {
+document.getElementById('loginButton').addEventListener('click', function(e) {
+  e.preventDefault();
   logIn();
 });
 
@@ -96,17 +117,70 @@ function logIn() {
 
   // Kiểm tra xem đã nhập đủ thông tin chưa
   if (email && password) {
-	var user = JSON.parse(localStorage.getItem('user'));
+    var users = JSON.parse(localStorage.getItem('users')) || [];
 
-	// Kiểm tra xem thông tin đăng nhập có chính xác hay không
-	if (user && user.email === email && user.password === password) {
-	  alert('Logged in successfully!');
-	  closePopup();
-	  // Thực hiện các hành động sau khi đăng nhập thành công
-	} else {
-	  alert('Email or password is incorrect.');
-	}
+    // Kiểm tra xem thông tin đăng nhập có chính xác hay không
+    var user = users.find(function(user) {
+      return user.email === email && user.password === password;
+    });
+
+    if (user) {
+      alert('Logged in successfully!');
+      setUsername(user.name);
+      toggleLoginLogout(true);
+      closePopup();
+      // Lưu thông tin người dùng đã đăng nhập vào localStorage
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
+      // Thực hiện các hành động sau khi đăng nhập thành công
+    } else {
+      alert('Email or password is incorrect.');
+    }
   } else {
-	alert('Please enter full information.');
+    alert('Please enter full information.');
   }
 }
+
+function setUsername(name) {
+  usernameElement.textContent = name;
+}
+
+function toggleLoginLogout(isLoggedIn) {
+  if (isLoggedIn) {
+    loginLink.style.display = 'none';
+    logoutLink.style.display = 'inline-block';
+  } else {
+    loginLink.style.display = 'inline-block';
+    logoutLink.style.display = 'none';
+  }
+}
+
+function logout() {
+  localStorage.removeItem('loggedInUser');
+  setUsername('');
+  toggleLoginLogout(false);
+  showLoginForm();
+}
+
+function showLoginForm() {
+  let parent = loginBtn.parentNode.parentNode;
+  Array.from(parent.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up')
+    } else {
+      signupBtn.parentNode.classList.add('slide-up')
+      parent.classList.remove('slide-up')
+    }
+  });
+}
+
+// Kiểm tra xem người dùng đã đăng nhập hay chưa khi tải trang
+document.addEventListener('DOMContentLoaded', function() {
+  var loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+  if (loggedInUser) {
+    setUsername(loggedInUser.name);
+    toggleLoginLogout(true);
+  } else {
+    toggleLoginLogout(false);
+  }
+});
